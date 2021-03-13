a3485e3eaf59ca2228d315da3f082adbcbd62ea6
pietere <ptre@saxobank.com>
2021-03-13 13:04:11 +0100

    Simplify

---
+++ pkg/internal/tools/tools.go
@@ -0,0 +1,9 @@
+// +build tools
+
+// Package tools lists the tools used by this project.
+package tools
+
+import (
+	_ "github.com/securego/gosec"
+	_ "golang.org/x/tools/cmd/stringer"
+)
--- tools.go
+++
@@ -1,8 +0,0 @@
-// +build tools
-
-// Package tools lists the tools that are invoked by go:generate.
-package tools
-
-import (
-	_ "golang.org/x/tools/cmd/stringer"
-)
